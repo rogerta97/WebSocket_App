2184c91a5934117bfa626469fc980ee74a2d11d2
Roger Tello Araez <rogerteello97@gmail.com>
2018-09-19 18:45:53 +0200

    Code Compiling

---
+++ Client/ClientMain.cpp
@@ -0,0 +1,65 @@
+#pragma comment(lib, "ws2_32.lib") 
+
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#define SOCKET_ERROR -1
+
+#include "Windows.h"
+#include "WinSock2.h"
+#include "Ws2tcpip.h"
+
+bool InitSockets()
+{
+	WSADATA wsaData;
+	int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != NO_ERROR)
+	{
+		// Log and handle error
+		if (WSAGetLastError() == SOCKET_ERROR)
+			return false;
+	}
+
+	return true;
+}
+
+bool CleanUpSockets()
+{
+	int iResult = WSACleanup();
+	return true;
+}
+
+SOCKET CreateUDPSocket()
+{
+	return socket(AF_INET, SOCK_DGRAM, 0);
+}
+
+SOCKET CreateTCPSocket()
+{
+	return socket(AF_INET, SOCK_STREAM, 0);
+}
+
+bool CloseSocket(SOCKET s)
+{
+	auto result = closesocket(s);
+
+	if (result == SOCKET_ERROR)
+		return false;
+
+	return true;
+}
+
+int main(int argc, char **argv)
+{
+	InitSockets();
+	SOCKET s = CreateUDPSocket();
+
+	struct sockaddr_in remoteAddr;
+	remoteAddr.sin_family = AF_INET;
+	remoteAddr.sin_port = htons(8000);
+	const char *remoteAddrStr = "127.0.0.1";
+	inet_pton(AF_INET, remoteAddrStr, &remoteAddr.sin_addr);
+
+	CloseSocket(s);
+	CleanUpSockets();
+}
+
